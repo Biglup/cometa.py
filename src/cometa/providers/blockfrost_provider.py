56570607608606f2ff1e8de9fc725981925d3582
Angel Castillo <angel.castillob@protonmail.com>
2025-12-17 23:02:47 +0800

    fix: removed lint warning

--- src/cometa/providers/blockfrost_provider.py
+++ src/cometa/providers/blockfrost_provider.py
@@ -274,7 +274,7 @@ class BlockfrostProvider:
     # Provider Protocol Implementation
     # -------------------------------------------------------------------------
 
-    def get_name(self) -> str:  # pylint: disable=no-self-use
+    def get_name(self) -> str:
         """Get the provider name."""
         return "Blockfrost"
 
